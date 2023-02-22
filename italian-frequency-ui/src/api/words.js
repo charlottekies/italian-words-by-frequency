ef2b6bd109d811727605049fb3e0e222748aa2d5
Charlotte Kies <chopekies@gmail.com>
2023-02-22 09:30:13 -0500

    Added .env again

--- italian-frequency-ui/src/api/words.js
+++ italian-frequency-ui/src/api/words.js
@@ -1,5 +1,5 @@
 // This is an example API module for getting words from the server
-const apiUrl = secrets.API_BASE_URL;
+const apiUrl = process.env.API_BASE_URL;
 
 export function getWords() {
   return fetch(apiUrl + "/word").then((response) => response.json());

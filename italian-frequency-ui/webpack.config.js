f110b6185919b9b11e834a0314486f484b1c43c1
Charlotte Kies <chopekies@gmail.com>
2023-02-22 12:10:32 -0500

    Updating webpack config

--- italian-frequency-ui/webpack.config.js
+++ italian-frequency-ui/webpack.config.js
@@ -1,7 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const DotenvPlugin = require("webpack-dotenv-plugin");
+const path = require("path");
 
 module.exports = {
   // ...
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
   plugins: [
     new DotenvPlugin({
       path: "./.env",
@@ -10,7 +17,11 @@ module.exports = {
       silent: true,
       defaults: false,
     }),
-    // ...
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
   ],
   // ...
 };

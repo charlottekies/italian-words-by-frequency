292929fe3ea3be0ff65428e5a003056d786ab75c
Charlotte Kies <chopekies@gmail.com>
2023-02-26 17:51:03 -0500

    Modified the name of the repo

--- italian-frequency-ui/deploy.sh
+++ italian-frequency-ui/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:charlottekies/italian-word-frequency.git master:gh-pages
+git push -f git@github.com:charlottekies/italian-words-by-frequency.git master:gh-pages
 cd -

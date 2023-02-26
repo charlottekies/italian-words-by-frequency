211798b00ad8211d692ca055088118d1629eb4c2
Charlotte Kies <chopekies@gmail.com>
2023-02-26 17:42:03 -0500

    Updated webpack and .env and api

--- italian-frequency-ui/vue.config.js
+++ italian-frequency-ui/vue.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require("@vue/cli-service");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/italian-frequency-ui/" : "/",
   transpileDependencies: true,
   configureWebpack: {
-    plugins: [new Dotenv()],
+    plugins: [
+      new Dotenv({
+        systemvars: true,
+      }),
+    ],
+    performance: {
+      maxAssetSize: 1000000, // or whatever size limit you want to set
+    },
   },
 });
